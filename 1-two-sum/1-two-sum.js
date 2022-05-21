8d9f63eb020dec9676e0373446f161fba07f5b32
Owen Yoshishige <owenyoshishige@gmail.com>
2022-05-21 14:53:11 -0700

    Update 1-two-sum
    Delete commented code.

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,7 @@
  * @return {number[]}
  */
 
-var twoSum = function(nums, target) {
-//   if (target === 0) {
-//       return [];
-//   }
-    
+var twoSum = function(nums, target) {    
   for (let i = 0; i < nums.length; i++) {
     let difference = target - nums[i];
     let restOfNums = nums.slice(i + 1, nums.length);
