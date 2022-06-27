42c81d86eedae46b35e1c9c6c498c4752dc66aba
Owen Yoshishige <owenyoshishige@gmail.com>
2022-06-27 10:01:07 -0700

    Time: 610 ms (9.50%), Space: 82.4 MB (12.48%) - LeetHub

--- 853-car-fleet/853-car-fleet.js
+++ 853-car-fleet/853-car-fleet.js
@@ -5,39 +5,6 @@
  * @return {number}
  */
 
-/*
-I - number, 2 arrays
-O - number
-C - none
-E - very large inputs, zero and one
-
-Psuedocode
-create result starting at 0
-
-create stack array with first car
-
-create cars array by mapping one of the arrays
-sort cars
-create result at one
-
-iterate the cars array starting at 1 
-  if speed of current car is less than or equal speed of car in stack
-    replace stack with current car
-    increment result
-  otherwise if greater
-    create sum1 as its position plus speed
-    create sum2 as its position plus speed
-    while sums are less than target
-      if sums are equal
-        push into stack
-        break;
-      increment both sums by their speed
-    if any of the sums are greater than target
-      replace stack with sum2
-    
-return result
-*/
-
 var carFleet = function(target, position, speed) {
   let cars = position.map((el, i) => [el, speed[i]]);
   cars.sort((a, b) => b[0] - a[0]);
