79e1ef4390cded550d10c7b6ab4516334e18952c
Owen Yoshishige <owenyoshishige@gmail.com>
2022-08-02 20:21:49 -0700

    Time: 364 ms (32.71%), Space: 98.3 MB (60.76%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -11,27 +11,6 @@
  * @return {number}
  */
 
-/*
-I- node
-O- number
-C- none
-E- if root is null
-
-base case: if current node is null, return null
-state: keep track of current depth
-
-Psuedocode
-if root is null return null
-
-traverse left
-traverse right
-
-if both are null, return current roots value
-
-return the minimum of the the left or right side
-
-*/
-
 var minDepth = function(root, depth = 1) {
   if (!root) return null;
     
