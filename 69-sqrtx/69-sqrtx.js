8fac8caa1b7fd4c6544908920f17e653ee3afe06
Owen Yoshishige <owenyoshishige@gmail.com>
2022-08-08 18:16:17 -0700

    Time: 79 ms (92.36%), Space: 43.5 MB (74.56%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -3,44 +3,6 @@
  * @return {number}
  */
 
-/*
-I - integer
-O - integer
-C - none
-E - if x null or number with no sqr root
-
-binary search a range of number
-
-between 1 and x is the range
-
-boundary based
-
-Pseudocode
-if x is falsey return x
-
-create start at 1
-create end at x
-create sqrt and set it to null
-
-
-while start is less than or equal to end
-  create mid by getting average of start and end
-  find product by multiplying mid by 2
-  
-  if product is equal to x
-    set sqrt to mid
-    break;
-  otherwise if product is greater than x
-    set sqrt to mid
-    set end to equal mid minus 1
-  otherwise
-    set sqrt to mid
-    set start to equal mid plus 1
-    
-  
-   return sqrt
-*/
-
 var mySqrt = function(x) {
   if (!x) return x;
     
