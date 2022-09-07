95e6269f7baafba3e886c51c9714667fc2b9764f
Owen Yoshishige <owenyoshishige@gmail.com>
2022-09-06 19:19:28 -0700

    Time: 128 ms (9.92%), Space: 43.2 MB (31.76%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -3,37 +3,6 @@
  * @return {string}
  */
 
-/*
-I- array of strings
-O- string
-C- none
-E- if array is empty
-
-Psuedocode
-if array length is empty, return empty string
-create common prefix string at set to zero
-create currentPrefix and set to current string
-create shortestString and set to infinite
-
-iterate the strs array
-  if current string length is less than shortestString
-    set shortestString to current string
-
-iterate with i and j until j reaches shortestString length, increment i
-  if i is equal to strs length
-    set common prefix to current prefix
-    set i to -1
-    increment j
-    continue
-  
-  add current char to current prefix
-  if current string does not start with current prefix
-    break;
-
-
-return common prefix vairable
-*/
-
 var longestCommonPrefix = function(strs) {
     if (!strs.length) return '';
     let commonPrefix = '';
