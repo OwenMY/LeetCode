9546f3b2b96c607e7e688ab519ea6d9ba1cac797
Owen Yoshishige <owenyoshishige@gmail.com>
2022-10-19 12:30:28 -0700

    Time: 178 ms (54.38%), Space: 48.6 MB (90.05%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -3,38 +3,6 @@
  * @return {number[][]}
  */
 
-/*
-I - Array of Arrays
-O - Array of Arrays
-C - none
-E - Graph with one node in it
-
-DFS
------
-States: path array
-Basecase: When the current arrays length is 0, push copy current path to result array
-Internal DFS
-
-Psuedocode
-if the graph has one node in it, return an array of array with 0
-create result array
-
-create dfs helper function
-  if current graphs node has length of 0
-    push copy of path array into result
-    return
-    
-  iterate the graphs current node
-    push current value into path
-    invoke dfs passing in current value and path
-    pop the path
-
-invoke dfs with 0 and array with a 0 inside arguements
-
-return result array
-*/
-
-
 var allPathsSourceTarget = function(graph) {
   if (!graph || !graph[0].length) return [[0]];
     
