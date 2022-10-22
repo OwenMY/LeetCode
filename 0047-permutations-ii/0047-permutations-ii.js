801a027abd6c939267b73fdbe1325a845e0809aa
Owen Yoshishige <owenyoshishige@gmail.com>
2022-10-21 18:55:31 -0700

    Time: 96 ms (85.94%), Space: 46.6 MB (48.08%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.js
+++ 0047-permutations-ii/0047-permutations-ii.js
@@ -3,40 +3,6 @@
  * @return {number[][]}
  */
 
-/*
-I - array of nums
-O - array of arrays of nums
-C - none
-E - constant repeating
-
-backtracking dfs 
-========================
-states: visited set, current combination
-basecase: when length of nums is reached in execution context
-
-Psuedocode
-if nums is empty, return an empty array
-
-create result array
-create memo which is a new map
-find n which is nums length
-
-create dfs function
-  if start index is equal to nums length
-    push copy of path into result array
-    return
-  
-  iterate input array starting at input start num and ending at nums array length plus 1
-    if cache does not contain current number at index 
-      push current number into path array
-      add current number and index to cache
-      invoke dfs passing in i plus 1 and path
-      pop current number from cache
-
-invoke dfs function
-return result array
-*/
-
 var permuteUnique = function(nums) {
   if (!nums.length) return [];
     
@@ -44,7 +10,6 @@ var permuteUnique = function(nums) {
   const n = nums.length;
   const memo = {};
   
-    
   const dfs = (path) => {
     if (path.length === n) {
       result.push([...path]);
