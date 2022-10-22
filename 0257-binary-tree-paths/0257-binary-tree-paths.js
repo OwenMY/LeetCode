570e4dfbf0490d84c9a3aa71c615812e4a341c21
Owen Yoshishige <owenyoshishige@gmail.com>
2022-10-22 16:37:02 -0700

    Time: 70 ms (90.52%), Space: 43.5 MB (81.14%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.js
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -11,33 +11,6 @@
  * @return {string[]}
  */
 
-/*
-I - root node
-O - array of strings
-C - none
-E - if root is empty, return empty. array
-
-DFS Backtracking
-states: current path
-basecase: when both left and right children are null
-
-Psuedocode
-if root is null, return an empty array
-
-create result array
-
-create dfs function
-
-  push current nodes value into path
-  if (root.left) dfs(root.left); 
-  if (root.right) dfs(root.right);
-  if (!root.left && !root.right) push the joining of path into the result array
-  pop current nodes value from path  
-  
-invoke dfs function
-return result
-*/
-
 var binaryTreePaths = function(root) {
   if (!root) return [];
     
